e91f5147ae472c40fc70a4ceaa112976f7e40f73
Alexandre Nascimento <a.evangelista@totvs.com.br>
2025-04-27 18:11:28 -0300

    Atualiza os manifests de build e de páginas para refletir a troca de rotas entre "Components" e "Relationships". Remove arquivos desnecessários e ajusta o hash do webpack para garantir a integridade do build. Modifica o sistema de gerenciamento de rotas para suportar as novas estruturas de página.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("96a97f8ebff1fab8")
+/******/ 		__webpack_require__.h = () => ("556349c48a89adfb")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
